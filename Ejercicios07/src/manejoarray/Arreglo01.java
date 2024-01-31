e202243640029964489f9b9f0f661df8ad42b4ec
unknown <apadilla2@utpl.edu.ec>
2024-01-31 15:54:57 -0500

    ejercicio en clases

--- Ejercicios07/src/manejoarray/Arreglo01.java
+++ Ejercicios07/src/manejoarray/Arreglo01.java
@@ -10,20 +10,20 @@ package manejoarray;
  * @author reroes
  */
 public class Arreglo01 {
+
     public static void main(String[] args) {
-        
+
         // creación de un arreglo
-        
         int[] arreglo = new int[5];
-        
+
         arreglo[0] = 10;
         arreglo[1] = 20;
         arreglo[2] = 30;
         arreglo[3] = 40;
         arreglo[4] = 50;
-        
-        // arreglo[5] = 60;
-        
+
+        arreglo[5] = 60;
+
         for (int i = 0; i < arreglo.length; i++) {
             System.out.println(arreglo[i]);
         }
